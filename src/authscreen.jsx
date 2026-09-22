d439ed20f378b571cb0d78a222af63bd4ed3d81f
Nkuranga Clement <nkurangaclement@gmail.com>
2026-09-22 20:33:44 +0200

    Fix login API URL

--- src/authscreen.jsx
+++ src/authscreen.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://verde-backend-w3gx.onrender.com";
 
 export default function AuthScreen({
   initialMode = "login",
